3e5c2db63fa567f1f9737627768fea0203e7f3df
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2025-02-07 14:38:57 -0300

    Inicio dos estudos de Java
    Primeira aula de Java na DIO

---
+++ src/Aula01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Aula01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Meu comentário
+        /*
+        * Meu comentário
+        * de
+        * várias linhas
+        * */
+
+        Scanner lerDados;
+    }
+}
