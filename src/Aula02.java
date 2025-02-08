6f33de0cc71fc63e3c5695dda02ef02c44c563f8
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2025-02-08 13:42:25 -0300

    Aula02 - Keywords e Tipos Primitivos
    Aula sobre as Keywords em Java e os Tipos Primitivos em Java

---
+++ src/Aula02.java
@@ -0,0 +1,18 @@
+// Primitive Types
+public class Aula02 {
+    public static void main(String[] args) {
+        /*
+        * byte - -128 - 127;
+        * short - -32768 - 32767;
+        * int - -2.147.483.648 - 2.147.483.647;
+        * long - -9.223.372.036.854.770.000 - 9.223.372.036.854.770.000;
+        *
+        * float
+        * double
+        *
+        * char (character);
+        *
+        * boolean = true, false
+        * */
+    }
+}
