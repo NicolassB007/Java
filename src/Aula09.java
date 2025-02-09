46424a0aafa3aa5972931c50b47819034fde7f98
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2025-02-09 12:32:41 -0300

    Estrutura While e Do While em Java

---
+++ src/Aula09.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Aula09 {
+    public static void main(String[] args) {
+        Scanner readData = new Scanner(System.in);
+        var name = "";
+        /*while (!name.equalsIgnoreCase("exit")) {
+            System.out.print("Informe um nome: ");
+            name = readData.nextLine();
+
+            System.out.println(name);
+        }*/
+
+        /*while (true) {
+            System.out.print("Informe um nome: ");
+            name = readData.nextLine();
+
+            System.out.println(name);
+            if (name.equalsIgnoreCase("exit")) break;
+        }*/
+
+        do {
+            System.out.print("Informe um nome: ");
+            name = readData.nextLine();
+
+            System.out.println(name);
+        } while (!name.equalsIgnoreCase("exit"));
+    }
+}
